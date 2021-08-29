37d8ef50200444335a46cfc19477e41859630cb7
ZJyoung1997 <1298462930@qq.com>
2021-08-29 16:32:21 +0800

    Revert "修改demo 配置"
    This reverts commit a9f70f061fdf980134da8ea3d892b1e0f9429006.

--- demo/src/main/java/com/jz/logger/demo/controller/BaseController.java
+++ demo/src/main/java/com/jz/logger/demo/controller/BaseController.java
@@ -8,7 +8,8 @@ import com.jz.logger.demo.pojo.TestData;
 public interface BaseController extends BaseInterface {
 
     @Logger(selectParam = "family?.id", selectMethod = "@mapper.get(#root[0])",
-            topic = "更新房主和朋友圈", methodParamConverter = AllArgsConverter.class)
+            topic = "更新房主和朋友圈", methodParamConverter = AllArgsConverter.class,
+            strategy = Strategy.ASYN_CONCURRENT)
     Object update(TestData data);
 
 }
