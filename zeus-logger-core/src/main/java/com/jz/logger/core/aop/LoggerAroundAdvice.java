e06b6885e9b6946d208da6690fa1451205ecf706
jiadi <di.jia@mail.hypers.com>
2021-09-16 17:02:05 +0800

    修复logger注解嵌套使用时无法正确获取查询参数的问题

--- zeus-logger-core/src/main/java/com/jz/logger/core/aop/LoggerAroundAdvice.java
+++ zeus-logger-core/src/main/java/com/jz/logger/core/aop/LoggerAroundAdvice.java
@@ -14,6 +14,7 @@ import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
+import java.util.LinkedList;
 
 /**
  * @Author JZ
@@ -22,11 +23,9 @@ import java.lang.reflect.Method;
 @Slf4j
 public class LoggerAroundAdvice implements MethodBeforeAdvice, AfterReturningAdvice {
 
-    private final ThreadLocal<Object> oldObject = new ThreadLocal<>();
+    private final ThreadLocal<LinkedList<Object>> oldObject = new ThreadLocal<>();
 
-    private final ThreadLocal<Object> newObject = new ThreadLocal<>();
-
-    private final ThreadLocal<Object> selectParam = new ThreadLocal<>();
+    private final ThreadLocal<LinkedList<Object>> newObject = new ThreadLocal<>();
 
     private final LoggerHandler loggerHandler;
 
@@ -38,15 +37,12 @@ public class LoggerAroundAdvice implements MethodBeforeAdvice, AfterReturningAdv
     @SneakyThrows
     public void before(Method method, Object[] args, Object targetObject) {
         Logger logger = MethodUtils.getMethodAnnotation(method, Logger.class);
+        init();
 
-        newObject.remove();
-        oldObject.remove();
-        selectParam.remove();
         if (logger.enabledManual()) {
             return;
         }
-        selectParam.set(getSelectParam(logger, args));
-        oldObject.set(SpelUtils.getValue(logger.selectMethod(), selectParam.get()));
+        oldObject.get().push(SpelUtils.getValue(logger.selectMethod(), getSelectParam(logger, args)));
     }
 
     @Override
@@ -54,15 +50,13 @@ public class LoggerAroundAdvice implements MethodBeforeAdvice, AfterReturningAdv
         Logger logger = MethodUtils.getMethodAnnotation(method, Logger.class);
         if (logger.enabledManual()) {
             if (LoggerHolder.isRecorded()) {
-                oldObject.set(LoggerHolder.getOldObject());
-                newObject.set(LoggerHolder.getNewObject());
-            } else {
-                throw new RuntimeException("无法记录日志");
+                oldObject.get().push(LoggerHolder.getOldObject());
+                newObject.get().push(LoggerHolder.getNewObject());
             }
         } else {
-            newObject.set(SpelUtils.getValue(logger.selectMethod(), selectParam.get()));
+            newObject.get().push(SpelUtils.getValue(logger.selectMethod(), getSelectParam(logger, args)));
         }
-        loggerHandler.handleLogger(oldObject.get(), newObject.get(), args, logger);
+        loggerHandler.handleLogger(oldObject.get().pop(), newObject.get().pop(), args, logger);
     }
 
     private Object getSelectParam(Logger logger, Object[] args) {
@@ -74,6 +68,15 @@ public class LoggerAroundAdvice implements MethodBeforeAdvice, AfterReturningAdv
         selectParam = SpelUtils.getValue(logger.selectParam(), selectParam);
         Converter converter = ClassUtils.getConverterInstance(logger.paramConverter());
         return converter.transfor(selectParam);
+    }
+
+    private void init() {
+        if (oldObject.get() == null) {
+            oldObject.set(new LinkedList<>());
+        }
+        if (newObject.get() == null) {
+            newObject.set(new LinkedList<>());
+        }
     }
 
 }
